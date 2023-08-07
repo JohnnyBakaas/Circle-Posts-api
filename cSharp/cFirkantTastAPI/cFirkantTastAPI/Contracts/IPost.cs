34171af78e35ae3f723fed4ff6e7b42911f94a5e
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-08-07 13:46:23 +0200

    LeBackend

---
+++ cSharp/cFirkantTastAPI/cFirkantTastAPI/Contracts/IPost.cs
@@ -0,0 +1,22 @@
+﻿namespace cFirkantTastAPI.Contracts
+{
+    public interface IPost
+    {
+        string Avatar { get; }
+        string DisplayName { get; }
+        string Handle { get; }
+        int Followers { get; }
+        string Content { get; }
+        int Comments { get; }
+        int Likes { get; }
+        int DisLikes { get; }
+        int Views { get; }
+
+        bool You { get; }
+        bool Following { get; }
+        bool Liking { get; }
+        bool DisLikeing { get; }
+        Guid Id { get; }
+
+    }
+}
